26f8ca9e14e15de2198e02ae5a7716ac1548d018
tahseen_IO <tahseen@mogiio.com>
2023-09-12 23:09:58 +0530

    sign in with google auth done

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,44 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router";
 
 export default function OAuth() {
+
+  const navigate = useNavigate()
+
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log('user signed =>', user);
+
+      //check if user not exists
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        });
+      }
+
+      navigate('/')
+    } catch (error) {
+      console.log('error signing =>', error);
+      toast.error("Couldn't authorize Google account")
+    }
+  }
+  
   return (
-    <button className="flex w-full items-center justify-center bg-red-600 text-white px-6 py-3 text-sm font-medium uppercase hover:bg-red-700 active:bg-red-800 shadow-md rounded-lg my-6 transition duration-200 ease-linear">
+    <button type="button" onClick={onGoogleClick} className="flex w-full items-center justify-center bg-red-600 text-white px-6 py-3 text-sm font-medium uppercase hover:bg-red-700 active:bg-red-800 shadow-md rounded-lg my-6 transition duration-200 ease-linear">
       {" "}
       <FcGoogle className="text-2xl bg-white rounded-full mr-4" />
       Continue With Google
